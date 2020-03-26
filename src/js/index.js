86ec97e62775054c0d2d706db41b4633554426c0
Santiago Rodríguez <santo1996.29@gmail.com>
2020-03-26 15:45:29 -0400

    fix code style errors in js/index

--- src/js/index.js
+++ src/js/index.js
@@ -1,14 +1,23 @@
+/* eslint-disable import/no-unresolved */
+
 import '@fortawesome/fontawesome-free/css/all.css';
 import '@fortawesome/fontawesome-free/js/all';
+
+/* eslint-enable import/no-unresolved */
+
 import '../scss/style.scss';
 import TabsComponent from './components/tabs/index';
 import HeaderComponent from './components/header/index';
 import FooterComponent from './components/footer/index';
 
-const head = document.head;
-const body = document.body;
-const charset = document.createElement('meta');
-const content = document.createElement('div');
+const {
+  head, body, charset, content,
+} = {
+  head: document.head,
+  body: document.body,
+  charset: document.createElement('meta'),
+  content: document.createElement('div'),
+};
 
 charset.setAttribute('charset', 'UTF-8');
 content.id = 'content';
