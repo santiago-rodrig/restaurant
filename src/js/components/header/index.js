bfb1658d362e4dcb446ae4ba70425c286963af46
Santiago Rodríguez <santo1996.29@gmail.com>
2020-03-26 11:29:39 -0400

    add margin to main header

--- src/js/components/header/index.js
+++ src/js/components/header/index.js
@@ -2,8 +2,9 @@ function headerComponent() {
   const header = document.createElement('header');
   const heading = document.createElement('h1');
 
-  heading.classList.add('text-center');
+  header.id = 'main-header';
   heading.innerText = 'Restaurant Logo';
+  heading.classList.add('text-center');
   header.appendChild(heading);
 
   return header;
